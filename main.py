604d19175dbed1cd7ca82c64ce5b1444e988bed1
HoloEye <patrykmitura98@gmail.com>
2022-04-21 23:34:13 +0200

    finding and warping the paper sheet

--- main.py
+++ main.py
@@ -15,3 +15,19 @@ scale = 3
 wP =210*scale
 hP = 297*scale
 
+
+while True:
+    if webcam:success, img = cap.read()
+    else: img = cv2.imread(path)
+
+    imgContours, finalContours =  functions.getContours(img,showCanny=False,minArea = 50000,filter=4,draw = False)
+
+
+
+    if len(finalContours) != 0:
+        biggest = finalContours[0][2]
+        
+        imgWarp = functions.warpImg(img,biggest,wP, hP)
+        cv2.imshow("A4", imgWarp)
+           if cv2.waitKey(1) & 0xFF == ord('q'):
+             break

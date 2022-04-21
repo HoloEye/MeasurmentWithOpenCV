a2b6fd2d7ba3188ca38b2a22def1ad65e3377dc9
HoloEye <patrykmitura98@gmail.com>
2022-04-21 23:36:40 +0200

    finding and measuring the rectangles

--- main.py
+++ main.py
@@ -29,5 +29,28 @@ while True:
         
         imgWarp = functions.warpImg(img,biggest,wP, hP)
         cv2.imshow("A4", imgWarp)
-           if cv2.waitKey(1) & 0xFF == ord('q'):
-             break
+        
+        imgContours2, finalContours2 = functions.getContours(imgWarp,cThreshold = [50, 50],showCanny=False, minArea=2000,filter=4,draw=False)
+
+    #
+    #
+        if len(finalContours) !=0:
+            for obj in finalContours2:
+                cv2.polylines(imgContours2,[obj[2]],True,(0,255,0),2)
+                nPoints = functions.reorder((obj[2]))
+                mW = round((functions.findDistance(nPoints[0][0] // scale,nPoints[1][0]//scale)/10),1)
+                hW = round((functions.findDistance(nPoints[0][0] // scale, nPoints[2][0] // scale) / 10), 1)
+
+                cv2.arrowedLine(imgContours2, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[1][0][0], nPoints[1][0][1]), (255, 0, 0), 3, 8, 0, 0.05)
+                cv2.arrowedLine(imgContours2, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[2][0][0], nPoints[2][0][1]), (255, 0, 0), 3, 8, 0, 0.05)
+
+                x,y,w,h = obj[3]
+                cv2.putText(imgContours2,'{}cm'.format(mW),(x+10,y-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.7,(255,0, 0),None)
+                cv2.putText(imgContours2, '{}cm'.format(hW), (x - 70, y + h//2), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.7, (255, 0, 0), None)
+        cv2.imshow("Measured", imgContours2)
+
+    cv2.imshow("Original", imgContours)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+         break
+
+    cv2.waitKey(1)
